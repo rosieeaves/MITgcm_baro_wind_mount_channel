13d32f51c17effffa0db1de4c68bb4edc1a186b5
Rosie Eaves <rosie.eaves@univ.ox.ac.uk>
2021-03-02 15:08:25 +0000

    Run for 10 years

---
+++ build/BUILD_INFO.h
@@ -0,0 +1,4 @@
+#define THISVER 'checkpoint67v'
+#define THISUSER 'eavesr'
+#define THISDATE 'Tue  2 Mar 10:33:44 GMT 2021'
+#define THISHOST 'atmlxint1'
--- input/genBathy.py
+++ input/genBathy.py
@@ -23,7 +23,7 @@ bathy = np.zeros((Nx,Ny))
 
 for i in range(Nx):
     for j in range(Ny):
-        if i!=0 and i!=Nx-1 and j!=0 and j!=Ny-1:
+        if j!=0 and j!=Ny-1:
             bathy[i][j] = -(H-h*np.sin((np.pi*x[i])/L)*np.sin((np.pi*y[j])/W)) 
 
 bathy = np.transpose(bathy)
